62f35f9260ace6aa732b9c351303a7f527babc2f
vinv-rcb <nguyenvi308.cntt@gmail.com>
2023-08-30 23:32:52 +0700

    rename and add test dependencies

--- src/main/java/com/example/demotraceingv2x/DemoTracingV2xApplication.java
+++ src/main/java/com/example/demotraceingv2x/DemoTracingV2xApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class DemoTraceingV2xApplication {
+public class DemoTracingV2xApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoTraceingV2xApplication.class, args);
+        SpringApplication.run(DemoTracingV2xApplication.class, args);
     }
 
 }
--- src/test/java/com/example/demotraceingv2x/DemoTracingV2xApplicationTests.java
+++ src/test/java/com/example/demotraceingv2x/DemoTracingV2xApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoTraceingV2xApplicationTests {
+class DemoTracingV2xApplicationTests {
 
     @Test
     void contextLoads() {
